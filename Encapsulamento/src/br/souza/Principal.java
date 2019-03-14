00ca7d79f8683289b8cea697fee9d4f298c0f7d8
Brendo Souza Pinheiro <brendo.pinheiro@ucsal.edu.br>
2019-03-14 17:36:55 -0300

    Encapsulamento (Controle Remoto)

---
+++ Encapsulamento/src/br/souza/Principal.java
@@ -0,0 +1,14 @@
+package br.souza;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		ControleRemoto c = new ControleRemoto();
+		c.ligar();
+		c.maisVolume();
+		c.play();
+		c.abrirMenu();
+		c.fecharMenu();
+	}
+
+}
